659fcac8d105ca227f2d73b24611780e1646651c
Rodrigo Tsuru <caixapostal@gmail.com>
2017-11-26 13:27:16 -0200

    test enhacement

--- src/test/java/cz/destil/settleup/AppTest.java
+++ src/test/java/cz/destil/settleup/AppTest.java
@@ -40,7 +40,7 @@ public class AppTest
     /**
      * Test mutual same amount payment
      */
-    public void testApp()
+    public void testMutualPayments()
     {
     	String currency = "BRL";
     	
@@ -52,6 +52,54 @@ public class AppTest
     	payments.add(new Payment("1","2","Pagamento","1","1",Calendar.getInstance().getTimeInMillis(),"BRL",false,1L,""));
     	
     	payments.add(new Payment("1","1","Pagamento","2","1",Calendar.getInstance().getTimeInMillis(),"BRL",false,1L,""));
+    	
+    	DebtCalculator.updateBalances(members, payments, currency);	
+    	
+    	BigDecimal tolerance = BigDecimal.valueOf(0);    	
+    	
+    	List<Debt> results = DebtCalculator.calculate(members, tolerance, currency);
+    	
+    	for (Debt debt : results) {
+			System.out.println("Debt: " + debt);
+		}
+    	
+        assertTrue( results.isEmpty() );
+    }
+    
+    /**
+     * Test mutual same amount payment
+     */
+    public void testApp()
+    {
+    	String currency = "BRL";
+    	
+    	List<Member> members = new ArrayList<Member>();
+    	members.add(new Member(1L,"Marcela","a@bc.com",1L,"Bank Account"));
+    	members.add(new Member(2L,"Shoiti","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(3L,"Miyuki","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(4L,"Sato","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(5L,"Celene","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(6L,"Yanai","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(7L,"Hidemi","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(8L,"Paula","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(9L,"Wlad","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(10L,"Shigueo","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(11L,"Mirian","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(12L,"Takae","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(13L,"Hanashiro","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(14L,"Tsuru","b@bc.com",1L,"Bank Account"));
+    	members.add(new Member(15L,"Priscilla","b@bc.com",1L,"Bank Account"));
+    	
+    	List<Payment> payments = new ArrayList<Payment>();
+    	payments.add(new Payment("59.8","1 2 3 4 5 6 7 8 9 10 11 12 13 14 15","Pagamento","1","1",Calendar.getInstance().getTimeInMillis(),"BRL",false,1L,""));
+    	
+    	payments.add(new Payment("216.76","1 2 3 4 5 6 7 8 9 10 11 12 13 14 15","Pagamento","2","1",Calendar.getInstance().getTimeInMillis(),"BRL",false,1L,""));
+    	
+    	payments.add(new Payment("338","1 2 3 4 5 6 7 8 9 10 11 12 13 14 15","Pagamento","4","1",Calendar.getInstance().getTimeInMillis(),"BRL",false,1L,""));
+    	
+    	payments.add(new Payment("277.73","1 2 3 4 5 6 7 8 9 10 11 12 13 14 15","Pagamento","6","1",Calendar.getInstance().getTimeInMillis(),"BRL",false,1L,""));
+    	
+    	payments.add(new Payment("263","1 2 3 4 5 6 7 8 9 10 11 12 13 14 15","Pagamento","8","1",Calendar.getInstance().getTimeInMillis(),"BRL",false,1L,""));
     	
     	DebtCalculator.updateBalances(members, payments, currency);	
     	
